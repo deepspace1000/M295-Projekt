67d94de1796cfbe58c7b721acf03e8e8ba125c0a
deepspace1000 <nilsrothe05@icloud.com>
2023-01-29 19:14:02 +0100

    Update registrieren.php

--- src/registrieren.php
+++ src/registrieren.php
@@ -1,8 +1,15 @@
 <?php session_start();
-    if (!isset($_SESSION['userid']) || $_SESSION['abteilung'] != 1 || $_POST['sub'] == "abbrechen"){
+    if (!isset($_SESSION['userid']) || $_SESSION['abteilung'] != 1 || $_POST['sub'] == "abbrechen" || !isset($_POST['sub'])){
         header("Location: index.php");
         die;
     }
+
+    $name = $_POST['name'];
+    $vorname = $_POST['vorname'];
+    $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $abt = $_POST['abt'];
+
+
 
     
 ?>
