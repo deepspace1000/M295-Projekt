54214e667fa47a197e50fd0d96bde3dcc14e738c
deepspace1000 <nilsrothe05@icloud.com>
2023-01-29 20:45:17 +0100

    Update header.php

--- src/header.php
+++ src/header.php
@@ -1,10 +1,7 @@
-
-<form action="logout.php" method="POST">
-    <table>
-        <tr>
-            <th><?php echo $_SESSION['vorname']; ?></th>
-            <th><?php echo $_SESSION['name']; ?></th>
-            <th><input type="submit" name="logout" value="Ausloggen"></th>
-        </tr>
-    </table>
-</form>
+<table>
+    <tr>
+        <th><?php echo $_SESSION['vorname']; ?></th>
+        <th><?php echo $_SESSION['name']; ?></th>
+        <th><button onclick="window.location.href = 'logout.php';">Ausloggen</button></th>
+    </tr>
+</table>
