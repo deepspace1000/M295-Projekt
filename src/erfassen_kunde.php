37c63a7dc2d93706e945049d90a994ff717b28e6
deepspace1000 <nilsrothe05@icloud.com>
2023-01-29 21:36:36 +0100

    Update erfassen_kunde.php

--- src/erfassen_kunde.php
+++ src/erfassen_kunde.php
@@ -14,19 +14,48 @@
 </head>
 <body>
     <h1>Kunde erfassen</h1>
-    <form>
+    <form action="registrieren_kunde.php" method="POST">
         <table>
             <tr>
                 <td><label for="name">Name</label></td>
                 <td><input type="text" id="name" name="kname" maxlength="25"></td>
             </tr>
             <tr>
-                <td><label for="vorname">Name</label></td>
+                <td><label for="vorname">Vorname</label></td>
                 <td><input type="text" id="vorname" name="kvorname" maxlength="25"></td>
             </tr>
             <tr>
-                <td><label for="name">Name</label></td>
-                <td><input type="text" id="name" name="kname" maxlength="25"></td>
+                <td><label for="geschlecht">Geschlecht</label></td>
+                <td>
+                    <select name="kgeschlecht" id="geschlecht">
+                        <option value="Herr">Herr</option>
+                        <option value="Frau">Frau</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="tele">Telefon</label></td>
+                <td><input type="text" id="tele" name="ktele" maxlength="15"></td>
+            </tr>
+            <tr>
+                <td><label for="natel">Natel</label></td>
+                <td><input type="text" id="natel" name="knatel" maxlength="15"></td>
+            </tr>
+            <tr>
+                <td><label for="adresse">Adresse</label></td>
+                <td><input type="text" id="adresse" name="kadresse" maxlength="50"></td>
+            </tr>
+            <tr>
+                <td><label for="plz">PLZ</label></td>
+                <td><input type="number" id="plz" name="kplz" maxlength="4"></td>
+            </tr>
+            <tr>
+                <td><label for="ort">Ort</label></td>
+                <td><input type="text" id="ort" name="kort" maxlength="25"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="sub" value="erfassen"></td>
+                <td><input type="submit" name="sub" value="abbrechen"></td>
             </tr>
         </table>
     </form>
