7d720ff60f637d50bc560ca735e1f25715d92e0d
Ale <alejandro.wenzler@gmail.com>
2023-01-29 15:13:01 +0100

    Update ansichtbereichsleiter.php

--- src/ansichtbereichsleiter.php
+++ src/ansichtbereichsleiter.php
@@ -1,5 +1,5 @@
 <?php session_start();
-    if (!isset($_SESSION['userid']) || !$_SESSION['abteilung'] == 2){
+    if (!isset($_SESSION['userid']) || $_SESSION['abteilung'] != 2){
         header("Location: index.php");
         die;
     }
