ff13b1c6dae7c95160113077ccdfd061b2f2cdb6
deepspace1000 <nilsrothe05@icloud.com>
2023-01-29 20:24:37 +0100

    Update erfassen.php

--- src/erfassen.php
+++ src/erfassen.php
@@ -1,4 +1,8 @@
-<?php
+<?php session_start();
+    if (!isset($_SESSION['userid']) || $_SESSION['abteilung'] != 1){
+        header("Location: index.php");
+        die;
+    }
     if(isset($_POST['registrieren']))
     {
         header("Location: erfassen_mitarbeiter.php");
@@ -6,5 +10,8 @@
     elseif(isset($_POST['neuauftrag']))
     {
         header("Location: erfassen_auftrag.php");
+    }
+    else{
+        header("Location: index.php");
     }
 ?>
