cb81c258a65a18e3f239829e73d80dbea6fb452c
deepspace1000 <nilsrothe05@icloud.com>
2023-01-29 21:06:06 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,12 +16,31 @@
     <h1>Login</h1>
 
     <form action="login.php" method="POST">
-        <p>User ID</p>
-        <input type="text" name="loginID"><br>
-        <p>Password</p>
-        <input type="password" name="loginPassword"><br>
-        <input type="submit" name="sub" value="Anmelden">
+        <table>
+            <tr>
+                <td>
+                    <label for="id">UserID: </label>
+                </td>
+                <td>
+                    <input type="text" id="id" name="loginID">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="password">Passwort: </label>
+                </td>
+                <td>
+                    <input type="password" id="password" name="loginPassword"><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="sub" value="Anmelden">
+                </td>
+            </tr>
+        </table>
     </form>
+    
     <?php
         if(isset($_SESSION['meldungen'])){
             echo "<p>" . $_SESSION['meldungen'] . "</p>";
