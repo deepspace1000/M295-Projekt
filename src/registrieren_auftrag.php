e44e5d5f0a4d047d6b3393f92c9215d36ecfe581
deepspace1000 <nilsrothe05@icloud.com>
2023-01-30 20:22:40 +0100

    Update registrieren_auftrag.php

--- src/registrieren_auftrag.php
+++ src/registrieren_auftrag.php
@@ -8,38 +8,33 @@
     {
         $_SESSION['meldungen'] = "Keinen Auftrag asugewählt";
         header("Location: erfassen_auftrag.php"); 
-    }
-    else
-    {
-            unset($_SESSION['meldungen']);
-            require_once "db_connection.php";
-
-            $datum = $_POST['date'];
-            $zeit = $_POST['time'];
-            $kunde = $_POST['Kunde'];
-            $checkbox = $_POST['arbeit'];
-            $chk = "";
-            $terminWunsch = $_POST['terminWunsch'];
-            $kommentar = $_POST['Kommentar'];
-
-            foreach($checkbox as $chkbox)
-            {
-                $chk .= $chkbox . ",";
-            }
-            
-            
-
-            $statement = $con->prepare("INSERT INTO auftraege (Datum, Zeit, Kunde, Terminwunsch, Arbeit, Beschreibung) VALUES ('$datum', $zeit, '$kunde', '$terminWunsch', '$chk', '$kommentar')");
-            
-            if($statement->execute()){
-                header("Location: ansichtadmin.php");
-            }
-            else{echo "Fehler Beim erstellen eines neuen Auftrags!!";}
+        die;
     }
 
    
+    unset($_SESSION['meldungen']);
+    require_once "db_connection.php";
 
+    $datum = $_POST['date'];
+    $zeit = $_POST['time'];
+    $kunde = $_POST['Kunde'];
+    $checkbox = $_POST['arbeit'];
+    $chk = "";
+    $terminWunsch = $_POST['terminWunsch'];
+    $kommentar = $_POST['Kommentar'];
 
-
+    foreach($checkbox as $chkbox)
+    {
+        $chk .= $chkbox . ",";
+    }
     
+    
+
+    $statement = $con->prepare("INSERT INTO auftraege (Datum, Zeit, Kunde, Terminwunsch, Arbeit, Beschreibung) VALUES ('$datum', $zeit, '$kunde', '$terminWunsch', '$chk', '$kommentar')");
+    
+    if($statement->execute()){
+        header("Location: ansichtadmin.php");
+    }
+    else{echo "Fehler Beim erstellen eines neuen Auftrags!!";}
+
 ?>
