7c664a7b6ebd5db0e907aee2ae42fece10939918
deepspace1000 <nilsrothe05@icloud.com>
2023-01-30 15:37:34 +0100

    Update ansichtbereichsleiter.php

--- src/ansichtbereichsleiter.php
+++ src/ansichtbereichsleiter.php
@@ -32,22 +32,26 @@
         ON Auftraege.Kunde = Kunden.KNR");
 
         $query->execute();
-
-        echo "<table>";
-        echo "<tr><th>AufNr</th><th>Datum</th><th>Zeit</th><th>Kunde</th><th>Mitarbeiter</th><th>Arbeit</th><th>Beschreibung</th></tr>";
-        while($row = $query->fetchObject()){
-            echo "<tr>";
-            echo "<td>" . $row->AuftragsNr . "</td>";
-            echo "<td>" . $row->Datum . "</td>";
-            echo "<td>" . $row->Zeit . "</td>";
-            echo "<td>" . $row->Kunden_Vorname . " " . $row->Kunden_Name . "</td>";
-            echo "<td>" . $row->Mitarbeiter_Vorname . " " . $row->Mitarbeiter_Name . "</td>";
-            echo "<td>" . $row->Arbeit . "</td>";
-            echo "<td>" . $row->Beschreibung . "</td>";
-            echo "</tr>";
-            
-        }
-        echo "</table>";
+        echo "<form acion='' method='POST'>";
+            echo "<table>";
+            echo "<tr><th>AufNr</th><th>Datum</th><th>Zeit</th><th>Kunde</th><th>Mitarbeiter</th><th>Arbeit</th><th>Beschreibung</th></tr>";
+            while($row = $query->fetchObject()){
+                
+                echo "<tr>";
+                echo "<td>" . $row->AuftragsNr . "</td>";
+                echo "<td>" . $row->Datum . "</td>";
+                echo "<td>" . $row->Zeit . "</td>";
+                echo "<td>" . $row->Kunden_Vorname . " " . $row->Kunden_Name . "</td>";
+                if(isset($row->Mitarbeiter)){
+                    echo "<td>" . $row->Mitarbeiter_Vorname . " " . $row->Mitarbeiter_Name . "</td>";
+                }
+                echo "<td>" . $row->Arbeit . "</td>";
+                echo "<td>" . $row->Beschreibung . "</td>";
+                echo "</tr>";
+                
+            }
+            echo "</table>";
+        echo "</form>";
 
         
     ?>
