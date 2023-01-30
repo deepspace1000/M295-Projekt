1c70a1b84652cff3e7b18badffa785b5ebdb6d3d
deepspace1000 <nilsrothe05@icloud.com>
2023-01-30 21:43:41 +0100

    Update ansichtmitarbeiter.php
    Jeder Mitarbeiter sieht nur die ihm zugeteilten aufträge

--- src/ansichtmitarbeiter.php
+++ src/ansichtmitarbeiter.php
@@ -17,6 +17,41 @@
         <?php include "header.php";?>
     </header>
     <h1>Mitarbeiter</h1>
+
+    <?php
+        require_once "db_connection.php";
+        $user = $_SESSION['userid'];
+
+        //$query = "SELECT * FROM Mitarbeiter WHERE MNR = '$loginID'";
+        $query = $con->prepare(
+        "SELECT * 
+        FROM Auftraege
+        LEFT JOIN Mitarbeiter
+        ON Auftraege.Mitarbeiter = Mitarbeiter.MNR
+        LEFT JOIN Kunden
+        ON Auftraege.Kunde = Kunden.KNR
+        WHERE Auftraege.Mitarbeiter = $user");
+
+        $query->execute();
+
+        echo "<table>";
+        echo "<tr><th>AufNr</th><th>Datum</th><th>Zeit</th><th>Kunde</th><th>Mitarbeiter</th><th>Arbeit</th><th>Beschreibung</th></tr>";
+        while($row = $query->fetchObject()){
+            echo "<tr>";
+            echo "<td>" . $row->AuftragsNr . "</td>";
+            echo "<td>" . $row->Datum . "</td>";
+            echo "<td>" . $row->Zeit . "</td>";
+            echo "<td>" . $row->Kunden_Vorname . " " . $row->Kunden_Name . "</td>";
+            echo "<td>" . $row->Mitarbeiter_Vorname . " " . $row->Mitarbeiter_Name . "</td>";
+            echo "<td>" . $row->Arbeit . "</td>";
+            echo "<td>" . $row->Beschreibung . "</td>";
+            echo "</tr>";
+            
+        }
+        echo "</table>";
+
+        
+    ?>
     
 </body>
 </html>
