d18f70b9869a11adef37569704fbae3aabefe2fd
deepspace1000 <nilsrothe05@icloud.com>
2023-01-31 22:04:58 +0100

    Update pdf.php
    Abfragen fertig

--- src/pdf.php
+++ src/pdf.php
@@ -19,6 +19,7 @@
 
     $query->execute();
     $row = $query->fetchObject();
+    $arbeiten = explode(",", $row->Arbeit);
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +36,56 @@
     <p>Datum: <?php echo $row->Datum; ?></p>
     <p>Zeit: <?php echo $row->Zeit; ?></p>
     <p>Kunde/Kontaktperson:</p>
-    <p><?php echo $row->Geschlecht . " " . $row->Kunden_Vorname . " " . $row->Kunden_Name . "<br>" . $row->Adresse . "<br>" . $row->PLZ . " " . $row ?></p>
+    <p><?php echo $row->Geschlecht . " " . $row->Kunden_Vorname . " " . $row->Kunden_Name . "<br>" . $row->Adresse . "<br>" . $row->PLZ . " " . $row->Ort; ?></p>
+    <p>Telefon: <?php echo $row->Telefon; ?></p>
+    <p>Natel : <?php echo $row->Natel; ?></p>
+    <p>Adresse Objekt: dito</p>
+    <p>Adresse Verrechnung: Da chasch genau das vom Pdf neh Shay</p>
+    <p>-----------------</p>
+    <p>Auszuführende Arbeiten: </p>
+    <?php
+        foreach($arbeiten as $value){
+            $checked = "";
+            if($value == "Reparatur"){
+                $checked = "checked='checked'";
+                break;
+            }
+        }
+        echo "<input type='checkbox' id='reperatur' disabled='disabled' $checked>";
+        echo "<lable for='reperatur'>Reperatur</lable>";
 
+        foreach($arbeiten as $value){
+            $checked = "";
+            if($value == "Sanitaer"){
+                $checked = "checked='checked'";
+                break;
+            }
+        }
+        echo "<input type='checkbox' id='sanitaer' disabled='disabled' $checked>";
+        echo "<lable for='sanitaer'>Sanitär</lable>";
+
+        foreach($arbeiten as $value){
+            $checked = "";
+            if($value == "Heizung"){
+                $checked = "checked='checked'";
+                break;
+            }
+        }
+        echo "<input type='checkbox' id='heizung' disabled='disabled' $checked>";
+        echo "<lable for='heizung'>Heizung</lable>";
+
+        foreach($arbeiten as $value){
+            $checked = "";
+            if($value == "Garantie"){
+                $checked = "checked='checked'";
+                break;
+            }
+        }
+        echo "<input type='checkbox' id='garantie' disabled='disabled' $checked>";
+        echo "<lable for='garantie'>Garantie</lable>";
+    ?>
+    <p><?php echo $row->Beschreibung; ?></p>
+    <p>-------------</p>
+    <p>Terminwunsch: <?php echo $row->Terminwunsch; ?></p>
 </body>
 </html>
