e78467edfc522f71b0f5129327a4249982826c65
deepspace1000 <nilsrothe05@icloud.com>
2023-02-03 07:42:38 +0100

    Update logout.php

--- src/logout.php
+++ src/logout.php
@@ -2,4 +2,10 @@
     session_destroy();
     header("Location: index.php");
     die;
+
+    /**
+     * dieses File kann im header von jedem User über einen Button ausgewählt werden <br>
+     * es beendet die Session in welcher alle User infos gespeichert sind und er wird danach zur index datei geleitet <br>
+     * bei welcher er sich neu anmelden kann.
+     */
 ?>
